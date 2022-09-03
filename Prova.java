46d787de90611b2cc043f4e3c02a1f472fb33657
Arthur Peixoto <98115467+Arthurpeixoto25@users.noreply.github.com>
2022-09-03 19:31:22 -0300

    Create Prova.java
    Classe Provas criada para atividade referente a matéria de Programação Orientada a Objetos do curso de Ciência da Computação na UFERSA

---
+++ Prova.java
@@ -0,0 +1,120 @@
+public class Prova {
+    package entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prova{
+private LocalDate dataCriacao;
+private Disciplina disciplina;
+private Questoes questoes;
+private int codigo;
+
+private void quantquestoes(int qntquestoes) {
+
+}
+
+
+List<Prova> provas = new ArrayList <Prova>();
+
+public List<Prova> buscarProvaPorDisciplina(Disciplina disciplina){
+    List<Prova> listaRetorno = new ArrayList <Prova>(); //Criando uma lista para salvar todos os elementos necessários na hora do return 
+
+    for(Prova provinha : provas){
+        if(provinha.disciplina.equals(disciplina)){ //fazendo o teste
+            System.out.println(provinha.disciplina);
+            System.out.println(provinha.questoes);
+            System.out.println(provinha.dataCriacao);
+
+            listaRetorno.add(provinha); //adicionando a provinha a lista de retorno
+        }
+
+
+    }
+    return listaRetorno;
+}
+
+
+public List<Prova> buscarProvaPorSemestre(LocalDate Dia){
+    List<Prova> listaRetorno = new ArrayList <Prova>();
+
+    for(Prova provinha : provas){
+        if(provinha.dataCriacao.equals(Dia)){
+            System.out.println(provinha.disciplina);
+            System.out.println(provinha.questoes);
+            System.out.println(provinha.dataCriacao);
+
+            listaRetorno.add(provinha);
+        } 
+    }
+    return listaRetorno;
+}
+
+public void cadastrarProva (int codigo, Disciplina disciplina, int qntquestoes, LocalDate data ){
+    int dificuldadeVezes;
+    if(codigo < 0 ||  disciplina == null || qntquestoes < 0 || data == null){
+            Prova provinhaNova = new Prova();
+            provinhaNova.disciplina = disciplina;
+            provinhaNova.dataCriacao = data;
+            quantquestoes(qntquestoes);
+
+            provas.add(provinhaNova);
+    }
+    else{
+    }
+} 
+
+public void removerProva (int codigo){
+            codigo--;
+            provas.remove(codigo);
+    }
+} 
+
+public void editarProva (int codigo, Disciplina disciplina, int qntquestoes, LocalDate data ){
+    int dificuldadeVezes;
+    if(codigo < 0 ||  disciplina == null || qntquestoes < 0 || data == null){
+            Prova provinhaNova = new Prova();
+            provinhaNova.codigo = codigo++;
+            provinhaNova.disciplina = disciplina;
+            provinhaNova.dataCriacao = data;
+            quantquestoes(qntquestoes);
+
+            provas.set(codigo, provinhaNova);
+    }
+    else{
+    }
+} 
+
+
+public List<Prova> gerarRelatório(){
+    List<Prova> listaRetorno = new ArrayList <Prova>();
+    for(Prova provinha : provas){
+            System.out.println(provinha.codigo);
+            System.out.println(provinha.disciplina);
+            System.out.println(provinha.questoes);
+            System.out.println(provinha.dataCriacao);
+
+            listaRetorno.add(provinha);
+        } 
+    return listaRetorno;
+}
+
+public List<Prova> buscarProvaPorCodigo(int codigo){
+    List<Prova> listaRetorno = new ArrayList <Prova>();
+
+    for(Prova provinha : provas){
+        if(provinha.codigo.equals(codigo)){
+            System.out.println(provinha.disciplina);
+            System.out.println(provinha.questoes);
+            System.out.println(provinha.dataCriacao);
+
+            listaRetorno.add(provinha);
+        } 
+    }
+    return listaRetorno;
+}
+
+
+
+}
