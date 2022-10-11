53b1c9af9f31c7019da7c9de002e25ce0717fc75
Gabriel Augusto <58897595+gabriel-am12@users.noreply.github.com>
2022-10-11 11:08:17 -0300

    :sparkles: feat: create DisciplinaBO in services

--- model/service/DisciplinaBO.java
+++ model/service/DisciplinaBO.java
@@ -14,9 +14,46 @@ public class DisciplinaBO {
 
     public boolean cadastrar(Disciplina disciplina) {
         ResultSet rs = dao.findBySpecifiedField(disciplina, "codigo");
+
         try {
             if(rs == null) || !(rs.next()) {
                 if(dao.inserir(disciplina) == true)
+                    return true;
+                    else return false;
+            }
+            else return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Disciplina> listar() {
+        List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+        
+        ResultSet rs - dao.findAll();
+        try {
+            while(rs.next()) {
+                Disciplina.disciplina = new Disciplina();
+                Disciplina.setCodigo(rs.getInt("codigo"));
+                Disciplina.setNome(rs.getString("nome"));
+                Disciplina.setAssunto(rs.getString("assunto"));
+
+                Disciplina.add(disciplina);
+            }
+            return disciplinas;
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return null;
+    }
+
+    public boolean alterar (Aluno aluno) {
+        ResultSet rs = dao.findBySpecifiedField(disciplina, "codigo");
+        
+        try {
+            if(rs != null && rs.next()) {
+                if(dao.deletar(disciplina) == true)
                     return true;
                     else return false;
             }
