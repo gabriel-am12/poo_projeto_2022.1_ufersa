8baf73129ca474611d344b8e3e405bf2bcbd5ea4
João Vitor <83482081+jovitif@users.noreply.github.com>
2022-10-13 21:01:49 -0300

    Criação das subclasses de Questoes

---
+++ model/entity/QuestoesSubjetivas.java
@@ -0,0 +1,27 @@
+package br.edu.ufersa.sistemageradordeprova.model.entities;
+
+public class QuestoesSubjetivas extends Questoes {
+	private String resposta;
+
+	public QuestoesSubjetivas(int codigo, int tipo, String enunciado, Disciplina disciplina, String assunto, String dificuldade, String resposta) {
+		super(codigo, tipo, enunciado, disciplina, assunto, dificuldade);
+		this.resposta = resposta;
+	}
+	
+	public QuestoesSubjetivas() {
+		
+	}
+
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		if (resposta == null || resposta.isEmpty()){
+			System.out.println("Dados não validados");
+		}else{
+			this.resposta = resposta;
+		}
+	}
+	
+}
