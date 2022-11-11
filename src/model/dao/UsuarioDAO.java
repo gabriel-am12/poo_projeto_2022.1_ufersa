827e0a154ae21a553c600df9fcdbcc76e7bd1783
João Vitor <joaosales911@gmail.com>
2022-11-11 00:23:59 -0300

    algumas alterações para inserir usuario no banco de dados

--- src/model/dao/UsuarioDAO.java
+++ src/model/dao/UsuarioDAO.java
@@ -3,6 +3,9 @@ package model.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.entity.Usuario;
 
@@ -17,7 +20,7 @@ public class UsuarioDAO extends BaseDao<Usuario>{
 			if(rs.next()) {
 				Usuario user = new Usuario();
 				usuario.setId_usuario(rs.getInt("id_usuario"));
-				usuario.setEmai1(rs.getString("email"));
+				usuario.setEmail(rs.getString("email"));
 				usuario.setNome(rs.getString("nome"));
 				usuario.setLogin(rs.getString("login"));
 				usuario.setSenha(rs.getString("senha"));
@@ -34,7 +37,7 @@ public class UsuarioDAO extends BaseDao<Usuario>{
     }
 
 	public boolean cadastrar (Usuario usuario) {
-		String sql = "INSERT INTO tb_usuario  (login,senha) VALUES (?,?);";
+		String sql = "INSERT INTO tb_usuario (login,senha) VALUES (?,?);";
 		try {
 			PreparedStatement pst = getConnection().prepareStatement(sql);
 			pst.setString(1, usuario.getLogin());
@@ -46,6 +49,72 @@ public class UsuarioDAO extends BaseDao<Usuario>{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
+		}
+	}
+	
+	public boolean inserir(Usuario usuario) {
+		String sql = "INSERT INTO tb_usuario (nome,email,login,senha) VALUES (?,?,?,?)";
+		try {
+			PreparedStatement pst = getConnection().prepareStatement(sql);
+			pst.setString(1, usuario.getNome());
+			pst.setString(2, usuario.getEmail());
+			pst.setString(3, usuario.getLogin());
+			pst.setString(4, usuario.getSenha());
+			pst.execute();
+			return true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean removerByLogin(Usuario usuario) {
+		String sql = "DELETE FROM tb_usuario WHERE login = ?";
+		try {
+			PreparedStatement pst = getConnection().prepareStatement(sql);
+			pst.setString(1, usuario.getLogin());
+			pst.executeUpdate();
+			return true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public List<Usuario> listar(){
+		String sql = "SELECT * FROM tb_usuario";
+		Statement st;
+		ResultSet rs;
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		try {
+			st = getConnection().createStatement();
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				Usuario vo = new Usuario();
+				vo.setId_usuario(rs.getInt("id"));
+				vo.setEmail(rs.getString("email"));
+				vo.setNome(rs.getString("nome"));
+				vo.setLogin(rs.getString("login"));
+				vo.setSenha(rs.getString("senha"));
+				usuarios.add(vo);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return usuarios;
+	}
+	
+	public void editar(Usuario vo) {
+		String sql = "UPDATE tb_usuario SET nome = ? WHERE id = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = getConnection().prepareStatement(sql);
+			ptst.setString(1, vo.getNome());
+			ptst.setInt(2, vo.getId_usuario());
+			ptst.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/model/entity/Usuario.java
+++ src/model/entity/Usuario.java
@@ -7,19 +7,27 @@ public class Usuario {
     private String login;
     private String senha;
 
+    //GARANTINDO A INTEGRIDADE DOS DADOS COM OS METODOS GETS E SETS
+    
     public int getId_usuario() {
         return id_usuario;
     }
 
     public void setId_usuario(int id_usuario) {
-        this.id_usuario = id_usuario;
+    	if(id_usuario < 0){
+		Math.abs(id_usuario);
+		this.id_usuario = id_usuario;
+		}else{
+		this.id_usuario = id_usuario;
+		}
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmai1(String email) {
+    public void setEmail(String email) {
+    	if (email != null && !email.isEmpty())
         this.email = email;
     }
 
@@ -28,6 +36,7 @@ public class Usuario {
     }
 
     public void setNome(String nome) {
+    	if(nome != null && !nome.isEmpty())
         this.nome = nome;
     }
 
@@ -36,6 +45,7 @@ public class Usuario {
     }
     
     public void setLogin(String login) {
+    	if(login != null && !login.isEmpty())
         this.login = login;
     }
 
@@ -44,7 +54,39 @@ public class Usuario {
     }
 
     public void setSenha(String senha) {
+    	if(senha != null && !senha.isEmpty())
         this.senha = senha;
     }
 
+    //CRIAÇÃO DE ALGUNS CONSTRUTORES
+    
+	public Usuario(int id_usuario, String email, String nome, String login, String senha) {
+		setId_usuario(id_usuario);
+		setEmail(email);
+		setNome(nome);
+		setLogin(login);
+		setSenha(senha);
+	}
+    
+    public Usuario() {
+    	
+    }
+
+    public Usuario(String login, String senha) {
+    	setLogin(login);
+    	setSenha(senha);
+    }
+    
+//METODO TOSTRING PARA PRINTAR TODAS AS INFORMAÇÕES NA TELA NA HOJE DOS TESTES
+    
+    public String toString() {
+    	String frase="";
+    	frase = "\nId = " + this.id_usuario;
+    	frase = frase + "\nNome = " + this.nome;
+    	frase = frase + "\nLogin = " + this.login;
+    	frase = frase + "\nSenha = " + this.senha;
+    	frase = frase + "\nEmail = " + this.email;
+    	return frase;
+    }
+    
 }
